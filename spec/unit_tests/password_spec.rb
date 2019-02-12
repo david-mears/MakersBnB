cf1bacecf23770bc1538f80e83924ae69baa9941
Brooke Woolley <brooke.woolley@gmail.com>
2019-02-12 11:35:42 +0000

    Passing tests for passwords

--- spec/unit_tests/password_spec.rb
+++ spec/unit_tests/password_spec.rb
@@ -1,8 +1,15 @@
 describe Password do
 
+  it 'database should return true for correct username and password' do
+    expect(described_class.check_password('sgreen', 'password123')).to eq(true)
+  end
 
-  it 'database should hold username' do
-    expect(described_class.check_password('sgreen', 'password123')).to eq(true)    
+  it 'database should return false for incorrect username' do
+    expect(described_class.check_password('wrongusername', 'password123')).to eq(false)
+  end
+
+  it 'database should return false for incorrect password' do
+    expect(described_class.check_password('sgreen', 'wrongpassword')).to eq(false)
   end
 
 end
