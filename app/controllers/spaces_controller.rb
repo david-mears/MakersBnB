7ea8dd32865862f80dd49b2809c8fd09918c7089
Simon Green <simon.green@thielsen.co.uk>
2019-02-13 12:13:25 +0000

    added space functionality

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,8 +3,7 @@ class SpacesController < ApplicationController
   end
 
   def create
-    @space = Space.create(name: params[:name], description: params[:description], price: params[:price])
-    p @space
+    Space.create(name: params[:name], description: params[:description], price: params[:price], user_id: 1)
     redirect_to '/spaces'
   end
 
