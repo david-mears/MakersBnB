c9b9445d9f58489bcffde52d10b6c845d1d7f882
Brooke Woolley <brooke.woolley@gmail.com>
2019-02-13 11:39:20 +0000

    Created skeleton add space view and updated controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,6 +2,12 @@ class SpacesController < ApplicationController
   def new
   end
 
+  def create
+    @space = Space.create(name: params[:name], description: params[:description], price: params[:price])
+    p @space
+    redirect_to '/spaces'
+  end
+
   def show
     @spaces = Space.all
     p Space.all
