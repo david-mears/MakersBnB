0567e22249e78b79cbf98642738f68c13a40feca
Simon Green <simon.green@thielsen.co.uk>
2019-02-15 13:41:10 +0000

    refactor validations

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,8 +1,9 @@
 class Space < ActiveRecord::Base
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
-  validates :user_id, presence: true
+  # validates :name, presence: true
+  # validates :description, presence: true
+  # validates :price, presence: true
+  # validates :user_id, presence: true
+  validates_presence_of :name, :description, :price, :user_id
 
   belongs_to :user
   has_many :availability
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ActiveRecord::Base
-  validates :forename, presence: true
-  validates :surname, presence: true
-  validates :username, presence: true
-  validates :password, presence: true
-  validates :email, presence: true
-
+  validates_presence_of :forename, :surname, :username, :password, :email
   has_many :availability
   has_many :space
 
